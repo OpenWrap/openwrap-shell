f3c75d65f39b9df1a119b51923babcd694a718f4
Sebastien Lambla <seb@serialseb.com>
2012-06-14 15:48:07 +0100

    Rename tests, fixes assemblyinfo

--- src/OpenWrap.Shell/Properties/AssemblyInfo.cs
+++ src/OpenWrap.Shell/Properties/AssemblyInfo.cs
@@ -1,12 +1,5 @@
 ﻿using System.Reflection;
 
 #pragma warning disable 1607
-[assembly: AssemblyTitle("OpenWrap Shell")]
-[assembly: AssemblyCompany("Caffeine IT / naughtyProd Limited")]
-[assembly: AssemblyProduct("OpenWrap package management")]
-[assembly: AssemblyCopyright("Copyright © naughtyProd Limited 2009-2011")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyProduct("OpenWrap Package Manager")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2.1.0.6")]
-[assembly: AssemblyFileVersion("2.1.0.6")]
-[assembly:AssemblyInformationalVersion("2.1.0-beta.1+6")]
